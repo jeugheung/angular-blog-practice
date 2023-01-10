13bde04f33ff1cc432ac28121e01c38a05fb1b8e
Andrey Kim <akart.dev@gmail.com>
2023-01-10 14:33:26 +0600

    feat: add func for crate post in db fb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import { Environment } from "./interface";
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyDWh0brecawteJqsstMnMvqyMgs8wWRzSI'
+  apiKey: 'AIzaSyDWh0brecawteJqsstMnMvqyMgs8wWRzSI',
+  fbDBUrl: 'https://angular-blog-65825-default-rtdb.firebaseio.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import { Environment } from "./interface";
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyDWh0brecawteJqsstMnMvqyMgs8wWRzSI'
+  apiKey: 'AIzaSyDWh0brecawteJqsstMnMvqyMgs8wWRzSI',
+  fbDBUrl: 'https://angular-blog-65825-default-rtdb.firebaseio.com/'
 };
 
 /*
