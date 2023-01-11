1ea21016298eb4a4354e1b2b2041cde54fe6c74e
Andrey Kim <akart.dev@gmail.com>
2023-01-11 13:17:14 +0600

    feat: add post table

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PostsService } from '../shared/post.service';
 import { AuthService } from '../shared/services/auth.service';
+import { Post } from '../shared/interfaces'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = []
+  pSub: Subscription
 
-  ngOnInit(): void {
+  constructor(private postsService:  PostsService) { }
+
+  ngOnInit() {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+
+  remove(id: string ) {
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
   }
 
 
